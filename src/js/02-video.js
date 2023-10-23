f80fcd8ce863ffe895f21f26a2a7e11c8853a4ad
Lakanna <annalakus@ukr.net>
2023-10-23 22:29:46 +0100

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
 
+
+
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+
+
+
+function onTimeUpdate(data) {
+    let currentTime = data.seconds;
+
+};
+   
+player.on('timeupdate', onTimeUpdate); 
+
+player.setCurrentTime(30.456)
+       
